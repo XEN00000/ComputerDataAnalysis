a0778d1e0bf48ef581be4b58709b54d58ee7a3f3
251592 Murza Łukasz <251592@edu.p.lodz.pl>
2025-01-12 11:00:01 +0100

    Zadanie 3: algorytm zaimplementowany, wszystko śmiga wystarczy już tylko zrobić część wizualizacyjną

--- Zadanie 3/Zadanie.py
+++ Zadanie 3/Zadanie.py
@@ -1,29 +1,24 @@
-#       Samo użycie naszej biblioteki do rozwiązania zadania
 import numpy as np
-
 import lib_knn as knn
 
-data_train = np.array([
-    [5.1, 3.5, 1.4, 0.2, 0],
-    [5.2, 3.5, 1.5, 0.2, 0],
-    [4.6, 3.1, 1.5, 0.2, 0],
-    [6.7, 3.1, 4.4, 1.4, 1],
-    [5.2, 2.7, 3.9, 1.4, 1],
-    [6.9, 3.1, 5.1, 2.3, 2],
-    [7.7, 3.8, 6.7, 2.2, 2],
-    [7.2, 3.2, 6.0, 1.8, 2]
-])
 
-data_test = np.array([
-    [5.5, 4.2, 1.4, 0.2, 0],
-    [5.6, 2.7, 4.2, 1.3, 1],
-    [6.8, 3.0, 5.5, 2.1, 2]
-])
+data_train = np.loadtxt('data3_train.csv', delimiter=',')
+data_test = np.loadtxt('data3_test.csv', delimiter=',')
+
+# print(data_train)
+# print(data_train2)
+# print(data_test)
+# print(data_test2)
 
 knnSolver = knn.AlgorithmKNN(data_train)
 
 knnSolver.loadTestData(data_test)
-knnSolver.knn(3)
 
-#knnSolver.visualize()
-#knnSolver.missesMatrix()
+for i in range(1, 16):
+    knnSolver.knn(i)
+    knnSolver.visualize()
+    print(f"k = {i}, osiągnęło skuteczność: {knnSolver.accuracy()}%")
+# nnSolver.knn(4)
+# rint(f"{knnSolver.accuracy()}%")
+# knnSolver.visualize()
+# knnSolver.missesMatrix()
--- Zadanie 3/lib_knn.py
+++ Zadanie 3/lib_knn.py
@@ -1,11 +1,3 @@
-#       Tworzymy klasę która będzie rozwiązywać całe zadanie
-#       Nasza klasa będzie miała następujące rozwiązania:
-#       -> W konstruktorze przyjmnie od razu zbiór treningowy
-#       -> Metodę kitóra przyjmie zbiór testowy  zapełni tablicę odległości obiektów ze zbioru treningowego od obiektów ze zbioru testowego
-#       -> Metodę normalizującą dane (robimy to po to aby bardzo duże liczby np. na osi x nie zakłamywały wyniku gdy na osi y znajdą się bardzo małe np. Wykres zarobków i wieku osób)
-#       -> Metodę wyświetlającą wykres z rozwiązaniem
-#       -> Metodę wyświetlającą macierz pomyłek
-
 import numpy as np
 
 
@@ -15,49 +7,118 @@ class AlgorithmKNN:
     distanceMatrix = []  # Tablica odległości obiektów training od test
     trainingSetNormalized = []  # Zbiór treingowy znormalizowany (bez klas)
     testSetNormalized = []  # Zbiór testowy znormalizowany (bez klas)
+    expectedClasses = []    # Wyniki klasyfikacji przeprowadzonej przez program
 
     def __init__(self, trainingData):
         self.trainingSet = trainingData
         self.trainingSetNormalized = self.normalize(self.trainingSet[:, :-1])
 
-    @classmethod
-    def loadTestData(cls, testData):
-        cls.testSet = testData
-        cls.testSetNormalized = cls.normalize(cls.testSet[:, :-1])
+    def loadTestData(self, testData):
+        self.testSet = testData
+        self.testSetNormalized = self.normalize(self.testSet[:, :-1])
 
-    @classmethod
-    def knn(cls, k):
+    def knn(self, k):
         # krok 1.
-        cls.calculateDistances(cls.trainingSetNormalized, cls.testSetNormalized)
-        print(cls.distanceMatrix)
+        self.calculateDistances(self.trainingSetNormalized, self.testSetNormalized)
 
         # krok 2.
-        lowestInColumns = cls.findMin(cls.distanceMatrix, k)
+        lowestInColumns = self.findMin(self.distanceMatrix, k)
 
         # krok 3.
         # Teraz trzeba wyłuskać kolumny i wiersze najniższych odległości
         # Zapisać je w zmiennej żeby móc wyjąć sobie z trainingSet kolumny z klasami
         # Zobaczyć których jest najwięcej i przypisać klasy dla obiektów testowych
 
-    @classmethod
-    def __calculateDistances(cls, A, B):
-        #TODO: Do przejrzenia i zrozumienia oraz ewnetualnej optymalizacji
+        classesToSelect = self.fromDistanceToIndex(lowestInColumns)
+        self.expectedClasses = self.classesSelector(classesToSelect, k)
+        return
+
+    def calculateDistances(self, A, B):
+        # TODO: Do przejrzenia i zrozumienia oraz ewnetualnej optymalizacji
         A_suma_kwadratow = np.sum(A ** 2, axis=1).reshape(-1, 1)  # Wektor (m x 1)
         B_suma_kwadratow = np.sum(B ** 2, axis=1).reshape(1, -1)  # Wektor (1 x n)
 
         # Odległość euklidesowa: sqrt(|A|^2 + |B|^2 - 2*A*B.T)
         macierz_odleglosci = np.sqrt(A_suma_kwadratow + B_suma_kwadratow - 2 * np.dot(A, B.T))
 
-        cls.distanceMatrix = macierz_odleglosci
+        self.distanceMatrix = macierz_odleglosci
 
-    @classmethod
-    def __normalize(cls, x):
+    def fromDistanceToIndex(self, distances):
+        listOfObjectIndexes = []
+        for column, i in enumerate(distances):
+            sublist = []
+            for value, index in i:
+                sublist.append([column, int(self.trainingSet[index][4]), float(value)])
+                # print(f"Obiekt testowy nr: {column}   Klasa: {self.testSet[column][4]} <-> {self.trainingSet[index][4]}")
+                # print(f"Obiekt testowy nr: {column} -> {self.trainingSet[index][4]}")
+            listOfObjectIndexes.append(sublist)
+
+        return listOfObjectIndexes
+
+    def visualize(self):
+        return
+
+    def missesMatrix(self):
+        return
+
+    def accuracy(self):
+        classification_accuracy = 0
+        for number, category in enumerate(self.expectedClasses):
+            if category == self.testSet[number][4]:
+                classification_accuracy += 1
+
+        return round((classification_accuracy / len(self.testSet))*100, 2)
+
+    @staticmethod
+    def normalize(x):
         x_min = x.min(axis=0)
         x_max = x.max(axis=0)
         return (x - x_min) / (x_max - x_min)
 
-    @classmethod
-    def __findMin(cls, macierz, n):
+    @staticmethod
+    def classesSelector(classes, n):
+        result = []
+
+        for i in range(len(classes)):
+            votes = []
+            for j in range(n):
+                votes.append([classes[i][j][1], classes[i][j][2]])
+
+            #print(votes)
+
+            #   Liczymy nasze "głosy" korzystając ze słownika
+            counter = {}
+            weights = {}
+            for vote in votes:
+                if vote[0] in counter:
+                    counter[vote[0]] += 1
+                    weights[vote[0]] += float(1 / vote[1] + 1e-9)
+                else:
+                    counter[vote[0]] = 1
+                    weights[vote[0]] = float(1 / vote[1] + 1e-9)
+
+            #print(weights)
+            #print(counter)
+
+            winner = None
+            numberOfVotes = 0
+            for value, number in counter.items():
+                if number > numberOfVotes:
+                    numberOfVotes = number
+                    winner = value
+
+                if number == numberOfVotes:
+                    if weights[value] > weights[winner]:
+                        numberOfVotes = number
+                        winner = value
+
+            result.append(winner)
+
+        #print(result)
+        return result
+
+    @staticmethod
+    def findMin(macierz, n):
         # TODO: Przetłumaczyc wszystkie zmienne na ang
         wynik = []
         for j in range(macierz.shape[1]):  # Iterujemy po kolumnach
@@ -67,8 +128,8 @@ class AlgorithmKNN:
 
             # n oznacza nasze k obiektów do wybrania. Pętla przejdzie po kolumnie tyle razy ile mamy wybrać najmnieszych wartości z kolumny
             for _ in range(n):
-                min_wartosc = float(
-                    2.0)  # Inicjalizujemy wartość minimalną (2.0 dlatego że po normalizacji największą wartością będzie sqrt(2))
+                # Inicjalizujemy wartość minimalną (2.0 dlatego że po normalizacji największą wartością będzie sqrt(2))
+                min_wartosc = float(2.0)
                 min_indeks = -1
 
                 # Przeszukujemy całą kolumnę, aby znaleźć najmniejszy element
@@ -84,11 +145,3 @@ class AlgorithmKNN:
 
             wynik.append(najmniejsze)
         return wynik
-
-    @classmethod
-    def visualize(cls):
-        return
-
-    @classmethod
-    def missesMatrix(cls):
-        return
